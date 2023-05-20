f635e6ad36ed25ececb32f55fda87552a434396f
quocluu2k1 <quocluu2k1@gmail.com>
2023-05-20 23:10:15 +0700

    new update

---
+++ src/main/java/com/example/democloud/DemocloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.democloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocloudApplication.class, args);
+	}
+
+}
