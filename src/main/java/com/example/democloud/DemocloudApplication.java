7671ca2443e7e1c04473b12fd1afd082478e74e5
quocluu2k1 <91363200+quocluu2k1@users.noreply.github.com>
2023-05-21 01:37:58 +0700

    Update DemocloudApplication.java

--- src/main/java/com/example/democloud/DemocloudApplication.java
+++ src/main/java/com/example/democloud/DemocloudApplication.java
@@ -2,9 +2,17 @@ package com.example.democloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class DemocloudApplication {
+	
+	@GetMapping("/")
+    	public String demoString1(){
+        	return "Hello world!!!";
+    	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemocloudApplication.class, args);
